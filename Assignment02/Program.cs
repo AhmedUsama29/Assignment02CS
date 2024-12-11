0c41725ac9ce1e596509dbbeb66c601f0b863cad
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2024-12-11 16:31:22 +0200

    Solved Q3

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -24,28 +24,47 @@
 
             #region Q2 : Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
 
-            Console.WriteLine("Enter The Number: ");
-            String num = Console.ReadLine();
-            bool flag = int.TryParse(num, out int NewNum);
-            //didn't mention to handle zero
-            if (flag)
-            {
-                if (NewNum < 0)
-                {
-                    Console.WriteLine("Negative");
-                }
-                else
-                {
-                    Console.WriteLine("Posetive");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid Input");
-            }
+            //Console.WriteLine("Enter The Number: ");
+            //String num = Console.ReadLine();
+            //bool flag = int.TryParse(num, out int NewNum);
+            ////didn't mention to handle zero
+            //if (flag)
+            //{
+            //    if (NewNum < 0)
+            //    {
+            //        Console.WriteLine("Negative");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Posetive");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid Input");
+            //}
 
             #endregion
 
+            #region Q3 : Write a program that takes 3 integers from the user then prints the max element and the min element.
+            //no need to do big try parse [i understand]
+            Console.WriteLine("Enter the first number:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number:");
+            int num3 = int.Parse(Console.ReadLine());
+            
+            int max = Math.Max(num1, Math.Max(num2, num3));
+            int min = Math.Min(num1, Math.Min(num2, num3));
+
+            Console.WriteLine($"The maximum value is: {max}");
+            Console.WriteLine($"The minimum value is: {min}");
+
+
+            #endregion
         }
     }
 }
