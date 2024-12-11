b4b253cb1dbe3fc0b484a40d6abf30eeadeb0a7b
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2024-12-11 17:26:52 +0200

    Q11 Solved

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -200,35 +200,79 @@
 
             #region Q10 : Write a program to enter marks of five subjects and calculate total, average and percentage.
 
-            Console.WriteLine("Enter marks for 5 subjects:");
+            //Console.WriteLine("Enter marks for 5 subjects:");
 
-            int[] marks = new int[5];
-            int total = 0;
-            double average, percentage;
+            //int[] marks = new int[5];
+            //int total = 0;
+            //double average, percentage;
 
-            for (int i = 0; i < 5; i++)
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.Write($"Enter marks for subject {i + 1}: ");
+            //    bool flag = int.TryParse(Console.ReadLine(), out marks[i]);
+
+            //    if (!flag || marks[i] < 0)
+            //    {
+            //        Console.WriteLine("Please enter a valid non-negative value for marks");
+            //        return;
+            //    }
+
+            //    total += marks[i];
+            //}
+
+            //average = total / 5;
+            //percentage = (total / 500) * 100;
+
+            //Console.WriteLine($"\nTotal marks: {total}");
+            //Console.WriteLine($"Average marks: {average}");
+            //Console.WriteLine($"Percentage: {percentage}%");
+
+
+            #endregion
+
+            #region Q11 :  Write a program to input the month number and print the number of days in that month.
+
+            Console.WriteLine("Enter a number:");
+
+
+            bool flag = int.TryParse(Console.ReadLine(), out int month);
+
+            if (flag)
             {
-                Console.Write($"Enter marks for subject {i + 1}: ");
-                bool flag = int.TryParse(Console.ReadLine(), out marks[i]);
+                switch (month) {
 
-                if (!flag || marks[i] < 0)
-                {
-                    Console.WriteLine("Please enter a valid non-negative value for marks");
-                    return;
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                    case 12:
+                        Console.WriteLine("31 days");
+                        break;
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        Console.WriteLine("30 days");
+                        break;
+                    case 2:
+                        Console.WriteLine("28 or 29 days");
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid month number");
+                        break;
+
                 }
-
-                total += marks[i];
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number");
             }
 
-            average = total / 5;
-            percentage = (total / 500) * 100;
+            #endregion
 
-            Console.WriteLine($"\nTotal marks: {total}");
-            Console.WriteLine($"Average marks: {average}");
-            Console.WriteLine($"Percentage: {percentage}%");
         }
-
-        #endregion
     }
-    
+
 }
